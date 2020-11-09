5923a8519b758d780283b3b0461bae0511c038e5
mdyousuf77 <73985876+mdyousuf77@users.noreply.github.com>
2020-11-09 18:43:47 +0530

    Add files via upload

---
+++ positivenumbers.py
@@ -0,0 +1,13 @@
+list1=[12,-7,5,64,-14]
+print("input: lis1=",list1)
+print("output=")
+for num in list1:
+    if num>0:
+        print(num,end=" ")
+
+list2=[12,14,-95,3]
+print("\n input: list2=",list2)
+print("output=")
+for num1 in list2:
+    if num1>0:
+        print(num1,end=" ")
